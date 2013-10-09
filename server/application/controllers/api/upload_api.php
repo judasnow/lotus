3cd21b41db015b18822cc1ec95e71790c17606bc
odirus <odirus@163.com>
2013-10-09 20:24:35 +0000

    ...

---
+++ server/application/controllers/api/upload_api.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Apply api, Apply for shop
+ *
+ * @Author: odirus@163.com
+ */
+require_once(APPPATH . '/libraries/REST_Controller.php');
+require_once(APPPATH . '/libraries/auth.php');
+
+class Upload_api extends REST_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        session_start();
+    }
+
+    /**
+     * 验证用户是否已经登录
+     */
+    public function is_login() {
+        if(!auth::is_login()) {
+            $this->response(
+                array(
+                    'result' => 'fail',
+                    'msg'    => 'User did not login',
+                    'data'   => NULL
+                )
+            );
+        }
+    }
+
+
+    public function do_upload_image_post() {
+        $this->is_login();
+
+        $type = $this->input->post('image_type', TRUE);//可选值shop,product
+        
+        $this->load->library('upload_lib');
+        if ($this->upload_lib->do_upload_image($type)) {
+            $this->response(
+                array(
+                    'result' => 'ok',
+                    'msg'    => 'Upload image success',
+                    'data'   => NULL
+                )
+            );
+        } else {
+            $this->response(
+                array(
+                    'result' => 'fail',
+                    'msg'    => 'Upload image failed',
+                    'data'   => NULl
+                )
+            );
+        }
+    }
+
+}
--- server/application/models/product_model.php
+++ server/application/models/product_model.php
@@ -6,19 +6,40 @@
  */
 require_once('base_model.php');
 
-class Product_model() {
+class Product_model extends Base_model {
     
     private $_CI;
     private $_table = 'product';
 
     public function __construct() {
-        parent::__construct($thid->_table);
+        parent::__construct($this->_table);
         $this->_CI =& get_instance();
+    }
+
+    public function product($product) {
+        $res_object = $this->base_query(array('id' => $product['id']), '');
+        $res_array  = $res_object->result_array();
+        if ($this->result_rows($res_array) == 1) {
+            return $res_array[0];
+        } else {
+            return FALSE;
+        }
     }
 
     public function new_product($product_info) {
         $this->base_write($product_info);
-        if($this->affected_rows() == 1) {
+        if ($this->affected_rows() == 1) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function product_update($product) {
+        $product_id = $product['id'];
+        unset($product['id']);
+        $res_object = $this->base_update(array('id' => $product_id), $product);
+        if ($this->affected_rows() == 1) {
             return TRUE;
         } else {
             return FALSE;
