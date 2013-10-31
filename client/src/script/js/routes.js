d39911d0ee48badf181ba310419b658aa0d83023
judasnow <judasnow@gmail.com>
2013-10-31 15:48:37 +0800

    .

--- client/src/script/js/routes.js
+++ client/src/script/js/routes.js
@@ -21,7 +21,7 @@ define([
         },
 
         _showSellerSignup: function() {
-
+            
         }
     });
 
--- client/src/script/js/utility.js
+++ client/src/script/js/utility.js
@@ -3,5 +3,10 @@ define([
 ], function( $ ) {
     var utility = {};
 
+    //渲染一个新的 page 替换当前的 body
+    utility.loadPage = function(  ) {
+        
+    };
+
     return utility;
 });
