860b097f546b1ff48cf246231a5534b626e33d5c
judasnow <judasnow@gmail.com>
2013-11-28 00:59:25 +0800

    .

--- client/src/script/js/app/model/product.js
+++ client/src/script/js/app/model/product.js
@@ -11,8 +11,13 @@ define([
     'use strict';
 
     var Product = Backbone.Model.extend({
-        initialize: function() {
-            this.urlRoot = config.serverAddress + 'product_api/new_product/?session_id=' + common.getSessionId();
+        initialize: function( args ) {
+            if( typeof args.id !== 'undefined' && ! isNaN ( args.id ) ) {
+                this.set( 'id' , args.id );
+                this.url = config.serverAddress + 'product_api/product/?product_id=' + this.get( 'id' );
+            } else {
+                this.urlRoot = config.serverAddress + 'product_api/new_product/?session_id=' + common.getSessionId();
+            }
         }
     });
 
