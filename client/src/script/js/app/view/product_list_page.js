9101fe04c448b8fbe6e4fd862dca5ecfea3a2db4
judasnow <judasnow@gmail.com>
2013-12-02 15:43:10 +0800

    .

---
+++ client/src/script/js/app/view/product_list_page.js
@@ -0,0 +1,21 @@
+define([
+
+    'zepto',
+    'backbone',
+    'mustache',
+
+    'text!tpl/product_list_pager.mustache'
+
+] , function(
+    $,
+    Backbone,
+    Mustache,
+
+    productListPagerTpl
+
+) {
+    'use strict';
+
+    var 
+});
+
