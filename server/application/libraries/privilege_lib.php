2d47423daed40890a60f58cab1c85d530f22c33e
odirus <odirus@163.com>
2013-12-23 00:00:36 +0800

    ....

--- server/application/libraries/privilege_lib.php
+++ server/application/libraries/privilege_lib.php
@@ -10,6 +10,7 @@ class Privilege_lib {
 
     public function __construct() {
         $this->_CI =& get_instance();
+        $this->_CI->load->library('auth_lib');
     }
 
     public  function shop_privilege_user_id($shop_id) {
@@ -17,6 +18,10 @@ class Privilege_lib {
     }
 
     public function product_privilege_user_id($product_id) {
+        if (!$this->_CI->auth_lib->user_is_login()) {
+            //用户未登录，不具有认证权限
+            return false;
+        }
         $this->_CI->load->model('product_model', 'product_m');
         if ($privilege_user_id = $this->_CI->product_m->saler_id($product_id)) {
             if ($privilege_user_id == $_SESSION['object_user_id']) {
