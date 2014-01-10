9f427c8880de7efdb6d06b897bd46a651886692d
odirus <odirus@163.com>
2014-01-10 20:44:52 +0800

    ...

--- server/application/views/browser.php
+++ server/application/views/browser.php
@@ -7,6 +7,6 @@
 <body>
   <p>尊敬的用户：我们检测到您的浏览器内核为 IE8 或以下版本，为了更好地为您提供服务，请升级浏览器</p>
   <p>我们的建议：如果您是使用的360、搜狗、腾讯TT、猎豹等双核浏览器，请切换到极速模式，开始体验完善的服务，贸E街竭力为您提供最优质的服务</p>
-  <p>客户端下载：为了更好的提供服务，我们也提供本地客户端下载，下载地址 <a href="http://bcs.duapp.com/maoejie/maoejie.com.rar">点击这里</a>，解压文件夹，然后执行 maoejie.com.exe 文件即可</p>
+  <p>客户端下载：为了更好的提供服务，我们也提供本地客户端下载，下载地址 <a href="http://bcs.duapp.com/maoejie/maoejie.rar">点击这里</a>，解压文件夹，然后执行 maoejie.exe 文件即可</p>
 </body>
 </html>
