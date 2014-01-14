c62863794776dbe8ffdeef61b0faeb20b5f7dbf7
odirus <odirus@163.com>
2014-01-14 15:51:13 +0800

    ...

---
+++ weixin/index.php
@@ -0,0 +1,15 @@
+$signature = $_GET["signature"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];	
+
+$token = 'lotus';
+$tmpArr = array($token, $timestamp, $nonce);
+sort($tmpArr);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
+
+if( $tmpStr == $signature ){
+    return true;
+}else{
+    return false;
+}
